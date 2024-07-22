7c8b8897b907ca4cedb5b96cbfa40829283e8d3d
Jacob Foxe <fokkushi@gmail.com>
2024-07-22 15:58:20 -0400

    Added list_files_in_dir to Globals

--- Globals/Globals.gd
+++ Globals/Globals.gd
@@ -1,3 +1,19 @@
 extends Node
 
 const MAX_INGREDIENTS = 999	## Maximum number of ingredients. 
+
+## Lists all files in a directory.
+func list_files_in_directory(path) -> Array:
+	var files = []
+	var dir : DirAccess = DirAccess.open(path)
+	dir.list_dir_begin()
+
+	while true:
+		var file = dir.get_next()
+		if file == "":
+			break
+		elif not file.begins_with("."):
+			files.append(file)
+
+	dir.list_dir_end()
+	return files
