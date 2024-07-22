3e32b4030e1119ce6b7042b0801e33d249b40a3c
Jacob Foxe <fokkushi@gmail.com>
2024-07-22 13:51:18 -0400

    Added Main

---
+++ Main/Main.gd
@@ -0,0 +1,14 @@
+## This Component... 
+class_name Main extends Node
+
+## Parent Node
+var parent_node 
+
+
+func _ready():
+	pass
+
+
+## Set up parent's functionality for this component. 
+func setup_parent():
+	pass
