91f2c6827743f631fe8e9be182090b905d891a02
Jacob Foxe <fokkushi@gmail.com>
2024-07-22 14:52:08 -0400

    Removed template code

--- Main/Main.gd
+++ Main/Main.gd
@@ -1,14 +1,6 @@
-## This Component... 
+## The main game module
 class_name Main extends Node
-
-## Parent Node
-var parent_node 
-
 
 func _ready():
 	pass
 
-
-## Set up parent's functionality for this component. 
-func setup_parent():
-	pass
