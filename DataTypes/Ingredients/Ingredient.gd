e82b3647b2fcebb8ac316da6747233b5692d3274
Jacob <fokkushi@gmail.com>
2024-07-22 20:37:41 -0400

    Added png exports to ingredients and orders

--- DataTypes/Ingredients/Ingredient.gd
+++ DataTypes/Ingredients/Ingredient.gd
@@ -13,6 +13,7 @@ signal ingredient_gone
 @export var Name : String					## Name of the Ingredient
 @export var ID : int						## ID  of Ingredient
 @export_range(0, Globals.MAX_INGREDIENTS, 1) var Amount : int	## Amount of Ingredient
+@export_file("*.png") var imgRect			## Icon for this ingredient. 
 
 @export_group("Processing")
 @export var completedFlag : bool = false 	## TRUE if the ingredient is part of an order and it's been completed. 
