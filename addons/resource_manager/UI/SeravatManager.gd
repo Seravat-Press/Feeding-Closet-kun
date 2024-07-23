f73a74dfa1999b93f0fa61bb56beb3ad69492176
Jacob Foxe <fokkushi@gmail.com>
2024-07-23 10:01:50 -0400

    Fixed seravat manager again.

--- addons/resource_manager/UI/SeravatManager.gd
+++ addons/resource_manager/UI/SeravatManager.gd
@@ -1,14 +1,14 @@
-## Handles creating resources. 
+## Handles creating resources for the Seravat Press Pirate Jam 15 submission. 
 @tool
 class_name SeravatManager extends VBoxContainer
 
-const ingPath = preload("res://DataTypes/Ingredients/Ingredient.gd")
-const ordPath = preload("res://DataTypes/Order/Order.gd")
-const ingOutPath = "res://Data/Ingredients/"
-const ordOutPath = "res://Data/Orders/"
+const ingPath = preload("res://DataTypes/Ingredients/Ingredient.gd")	## Ingredient Resource.
+const ordPath = preload("res://DataTypes/Order/Order.gd")				## Order Resource. 
+const ingOutPath = "res://Data/Ingredients/"							## Output path for ingredients. 
+const ordOutPath = "res://Data/Orders/"									## Output path for orders.
 
-var ingText : String = "New [%3d]"
-var ordText : String = "New [%3d]"
+var ingText : String = "New [%3d]"	## Ingredient Button Text.
+var ordText : String = "New [%3d]"	## Order Button Text. 
 
 var resourceData : Resource		## Save Data Path. Setting up as a ResourceData causes a parse error. 
 
@@ -33,27 +33,53 @@ func update_button_labels() -> void:
 ## When the new Ingredient button is pressed, create a new Ingredient. 
 func _on_ing_button_pressed():
 	var newIngredient = ingPath.new()
-	var ingNamePath : String = ingOutPath + (str(resourceData.ingID) + "_" + ing_text.text) + ".tres"
+	
+	# Create the new ingredient name file/path. 
+	var ingNamePath : String = ingOutPath + (ing_text.text + "_" + str(resourceData.ingID)) + ".tres"
+	
+	# Set up the new ingredient data. 
 	newIngredient.Name = ing_text.text
 	newIngredient.Amount = 0
 	newIngredient.ID = resourceData.ingID
+	
+	# Save the new ingredient resource. 
 	resourceData.ingID += 1
 	ResourceSaver.save(newIngredient, ingNamePath)
+	
+	# Update the button labels and clear the ingredient lineedit. 
 	update_button_labels()
 	ing_text.text = ""
+	
+	# Open the resource in the inspector window. 
 	EditorInterface.get_inspector().resource_selected.emit(newIngredient, ingNamePath)
+	_save_resource_data()
 
 ## When the new Order button is pressed, create a new Order. 
 func _on_ord_button_pressed():
 	var newOrder = ordPath.new()
-	var ordNamePath : String = ordOutPath + (str(resourceData.orderID) + "_" + ord_text.text) + ".tres"
+	
+	# Create the new order name file/path. 
+	var ordNamePath : String = ordOutPath + (ord_text.text + "_" + str(resourceData.orderID)) + ".tres"
+	
+	# Set up the new order data. 
 	newOrder.Name = ord_text.text
 	newOrder.ID = resourceData.orderID
+	
+	# Save the new order resource. 
 	resourceData.orderID += 1
 	ResourceSaver.save(newOrder, ordNamePath)
+	
+	# Update the button labels and clear the order lineedit. 
 	update_button_labels()
 	ord_text.text = ""
+	
+	# Open the resource in the inspector window. 
 	EditorInterface.get_inspector().resource_selected.emit(newOrder, ordNamePath)
+	_save_resource_data()
 
-func _exit_tree():
+## Call to save the resource data. 
+func _save_resource_data() -> void:
 	ResourceSaver.save(resourceData, "res://addons/resource_manager/resourceData.tres")
+	
+func _exit_tree():
+	_save_resource_data()
