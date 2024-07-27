823b45eabd0b0387c0230c8f2142cd3bd8d9ce55
Jacob <fokkushi@gmail.com>
2024-07-26 21:21:52 -0400

    Added variable shadometer usage

--- UI/Closet/VisualHungerTimer.gd
+++ UI/Closet/VisualHungerTimer.gd
@@ -1,12 +1,10 @@
 ## UI Element to track the hunger timer. 
 class_name VisualHungerTimer extends ProgressBar
 
-@export var hunger_timer : Timer		## Connect to a timer node in the tree. 
-
 # Called when the node enters the scene tree for the first time.
-func _ready():
-	max_value = hunger_timer.wait_time
-	value = hunger_timer.wait_time
+func install_hunger_values(maxDuration : float):
+	max_value = maxDuration
+	value = maxDuration
 
 ## Updates the hunger timer value.
 func update_hunger_timer(newValue : float) -> void: 
