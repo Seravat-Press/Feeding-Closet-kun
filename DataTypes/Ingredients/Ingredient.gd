72c4016ea3b1c9ec51aea0c66d7032ac48775037
Jacob Foxe <fokkushi@gmail.com>
2024-07-27 11:39:15 -0400

    Added modifiers to ingredients

--- DataTypes/Ingredients/Ingredient.gd
+++ DataTypes/Ingredients/Ingredient.gd
@@ -5,5 +5,8 @@ class_name Ingredient extends Resource
 @export var ID : int						## ID  of Ingredient
 @export_file("*.png") var imgRect			## Icon for this ingredient. 
 
-
+@export_group("Modifiers")
+@export_range(0.01, 20.0, 0.01) var orderTimeMod : float = 0.01	## Modifier for amt of time per 1 in order
+@export_range(0.01, 20.0, 0.01) var orderCostMod : float = 0.01	## Modifier for amt cs per 1 in order
+@export_range(0.01, 20.0, 0.01) var fetchTimeMod : float = 0.01	## Modifier for amt of time per 1 in fetch
 
