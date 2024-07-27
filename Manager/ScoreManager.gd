92a1fd8366d8da9dfa18c1d90e6e6250124c7c25
Jacob <fokkushi@gmail.com>
2024-07-27 12:34:36 -0400

    Cleaned up scoremanager

--- Manager/ScoreManager.gd
+++ Manager/ScoreManager.gd
@@ -1,14 +1,10 @@
 ## Tracks and updates the high score. 
 class_name ScoreManager extends Node
 
-signal new_high_score(score)
-
 var currentHighScore : float = 0.0
 
 var happyPeople : Array[String]
 var sadPeople : Array[String]
-
-# TODO tie to end game screen
 
 ## Checks a new score against the current high score. 
 func check_new_score(newScore : float) -> bool:
@@ -17,8 +13,10 @@ func check_new_score(newScore : float) -> bool:
 		return true
 	return false
 
+## Add a happy person to the list.
 func add_happy_person(newName : String) -> void:
 	happyPeople.append(newName)
 
+## Add a sad person to the list. 
 func add_sad_person(newName : String) -> void:
 	sadPeople.append(newName)
